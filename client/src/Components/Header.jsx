162621ce95225274fa033d6aad0e9822105f053b
Fawad-R <115485767+Fawad-R@users.noreply.github.com>
2023-03-16 10:34:31 +0500

    4rth commit

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -32,10 +32,10 @@ const Header = () => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
-                // Setting various property values
+              // Setting various property values
                 let alink = document.createElement('a');
                 alink.href = fileURL;
-                alink.download = 'FawadRehmanCV.pdf';
+                alink.download = 'FawadRehman.pdf';
                 alink.click();
             })
         })
